91b45d380699e183b7f227244267bf2726e1199c
Gsbirds <gabbyburgard@yahoo.com>
2024-12-21 07:24:08 -0700

    perfecting main menu

--- MainGame.cs
+++ MainGame.cs
@@ -1,122 +1,120 @@
-// using Microsoft.Xna.Framework;
-// using Microsoft.Xna.Framework.Graphics;
-// using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
-// namespace monogame
-// {
-//     public class MainGame : Game
-//     {
-//         private GraphicsDeviceManager _graphics;
-//         private SpriteBatch _spriteBatch;
-//         private Texture2D mainmenu;
-//         private Rectangle startButtonArea;
-//         private Rectangle endButtonArea;
-//         private IGameState currentGameState;
-//         private bool inMainMenu = true;
+namespace monogame
+{
+    public class MainGame : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Texture2D mainmenu;
+        private Rectangle startButtonArea;
+        private Rectangle endButtonArea;
+        private IGameState currentGameState;
+        private bool inMainMenu = true;
 
-//         public enum GameStateType
-//         {
-//             MainMenu,
-//             Game1,
-//             Game2
-//         }
+        public enum GameStateType
+        {
+            MainMenu,
+            Game1,
+            Game2
+        }
 
-//         public MainGame()
-//         {
-//             _graphics = new GraphicsDeviceManager(this);
-//             Content.RootDirectory = "Content";
-//             IsMouseVisible = true;
-//         }
+        public MainGame()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
 
-//         protected override void Initialize()
-//         {
-//             _graphics.PreferredBackBufferWidth = 800;
-//             _graphics.PreferredBackBufferHeight = 600;
-//             _graphics.ApplyChanges();
+        protected override void Initialize()
+        {
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.PreferredBackBufferHeight = 600;
+            _graphics.ApplyChanges();
 
-//             base.Initialize();
-//         }
+            base.Initialize();
+        }
 
-//         protected override void LoadContent()
-//         {
-//             _spriteBatch = new SpriteBatch(GraphicsDevice);
-//             mainmenu = Content.Load<Texture2D>("mainmenudonut");
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            mainmenu = Content.Load<Texture2D>("mainmenudonut4");
 
-//             // Define the areas for "Start" and "End" buttons based on the menu image
-//             startButtonArea = new Rectangle(300, 250, 200, 50); // Adjust these values based on the image
-//             endButtonArea = new Rectangle(300, 320, 200, 50);
+            startButtonArea = new Rectangle(300, 250, 200, 50);
+            endButtonArea = new Rectangle(300, 320, 200, 50);
 
-//             // Initially in the main menu
-//             SwitchGameState(GameStateType.MainMenu);
-//         }
+            SwitchGameState(GameStateType.MainMenu);
+        }
 
-//         public void SwitchGameState(GameStateType newState)
-//         {
-//             if (newState == GameStateType.MainMenu)
-//             {
-//                 inMainMenu = true;
-//                 currentGameState = null;
-//             }
-//             else
-//             {
-//                 inMainMenu = false;
-//                 switch (newState)
-//                 {
-//                     // case GameStateType.Game1:
-//                     //     currentGameState = new Game1(this, _spriteBatch);
-//                     //     break;
-//                     case GameStateType.Game2:
-//                         currentGameState = new Game2(this, _spriteBatch);
-//                         break;
-//                 }
-//                 currentGameState.LoadContent();
-//             }
-//         }
+        public void SwitchGameState(GameStateType newState)
+        {
+            if (newState == GameStateType.MainMenu)
+            {
+                inMainMenu = true;
+                currentGameState = null;
+            }
+            else
+            {
+                inMainMenu = false;
+                switch (newState)
+                {
+                    // case GameStateType.Game1:
+                    //     currentGameState = new Game1(this, _spriteBatch);
+                    //     break;
+                    case GameStateType.Game2:
+                        currentGameState = new Game2(this, _spriteBatch);
+                        break;
+                }
+                currentGameState.LoadContent();
+            }
+        }
 
-//         protected override void Update(GameTime gameTime)
-//         {
-//             if (inMainMenu)
-//             {
-//                 MouseState mouseState = Mouse.GetState();
-//                 if (mouseState.LeftButton == ButtonState.Pressed)
-//                 {
-//                     Point mousePosition = mouseState.Position;
-//                     if (startButtonArea.Contains(mousePosition))
-//                     {
-//                         SwitchGameState(GameStateType.Game2); // Start the game
-//                     }
-//                     else if (endButtonArea.Contains(mousePosition))
-//                     {
-//                         Exit(); // Close the game
-//                     }
-//                 }
-//             }
-//             else
-//             {
-//                 currentGameState?.Update(gameTime);
-//             }
+        protected override void Update(GameTime gameTime)
+        {
+            if (inMainMenu)
+            {
+                MouseState mouseState = Mouse.GetState();
+                if (mouseState.LeftButton == ButtonState.Pressed)
+                {
+                    Point mousePosition = mouseState.Position;
+                    if (startButtonArea.Contains(mousePosition))
+                    {
+                        SwitchGameState(GameStateType.Game2);
+                    }
+                    else if (endButtonArea.Contains(mousePosition))
+                    {
+                        Exit();
+                    }
+                }
+            }
+            else
+            {
+                currentGameState?.Update(gameTime);
+            }
 
-//             base.Update(gameTime);
-//         }
+            base.Update(gameTime);
+        }
 
-//         protected override void Draw(GameTime gameTime)
-//         {
-//             GraphicsDevice.Clear(Color.BlanchedAlmond);
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.BlanchedAlmond);
 
-//             _spriteBatch.Begin();
+            _spriteBatch.Begin();
 
-//             if (inMainMenu)
-//             {
-//                 _spriteBatch.Draw(mainmenu, new Rectangle(60, -10, 650, 650), Color.White);
-//             }
-//             else
-//             {
-//                 currentGameState?.Draw(gameTime); 
-//             }
+            if (inMainMenu)
+            {
+                _spriteBatch.Draw(mainmenu, new Rectangle(60, -10, 650, 650), Color.White);
+            }
+            else
+            {
+                currentGameState?.Draw(gameTime); 
+            }
 
-//             _spriteBatch.End();
+            _spriteBatch.End();
 
-//             base.Draw(gameTime);
-//         }
-//     }
-// }
+            base.Draw(gameTime);
+        }
+    }
+}
--- obj/Debug/net8.0/monogame.AssemblyInfo.cs
+++ obj/Debug/net8.0/monogame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("monogame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ea74f733322a8df57e97d3b7979a7bdc287d6a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3630926fd8f6e6f92b6f17aeea23a813dfcc1c2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("monogame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("monogame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
